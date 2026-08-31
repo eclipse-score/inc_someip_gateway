02ddca81225a65f215f09b093925ddbb35c61a32
Lutz Reinhardt <lutz.reinhardt@elektrobit.com>
2026-08-31 15:08:21 +0000

    fix clang-tidy findings

--- score/gateway_ipc_binding/benchmark/read_only_memory_managers_benchmark.cpp
+++ score/gateway_ipc_binding/benchmark/read_only_memory_managers_benchmark.cpp
@@ -69,7 +69,7 @@ void benchmark_cached_read_only_lookup(benchmark::State& state) {
     auto& warmup = managers.get_read_only_shared_memory_slot_manager(metadata);
     benchmark::DoNotOptimize(&warmup);
 
-    for (auto _ : state) {
+    for (auto const& _ : state) {
         auto& manager = managers.get_read_only_shared_memory_slot_manager(metadata);
         benchmark::DoNotOptimize(&manager);
     }
